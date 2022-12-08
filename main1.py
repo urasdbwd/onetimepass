44b248a2ba53646b92dbfde684970a26558d5b08
Leo Liang <leozc@leozc.com>
2022-12-07 22:51:15 -0800

    first commit

---
+++ main1.py
@@ -0,0 +1,29 @@
+from flask import Flask, request
+import pyotp
+
+app = Flask(__name__)
+
+# Generate a new random secret key
+# The secret key should be shared between the user and the server
+secret_key = pyotp.random_base32()
+
+
+from flask import jsonify
+
+
+@app.route('/generate_otp/<secret_key>', methods=['POST','GET'])
+def generate_otp(secret_key):
+    # Generate a new OTP for the current time
+    totp = pyotp.TOTP(secret_key)
+    otp = totp.now()
+
+    # Return the OTP to the client as a JSON object
+    response = jsonify({'otp': otp})
+    response.headers['Access-Control-Allow-Origin'] = '*'
+
+    return response
+
+
+
+if __name__ == '__main__':
+    app.run()
