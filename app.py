ea502d28367c4d949fbeb304376784b95fb064c2
urasdbwd <102772348+urasdbwd@users.noreply.github.com>
2022-12-09 11:00:36 -0800

    Update app.py

--- app.py
+++ app.py
@@ -23,10 +23,5 @@ def generate_otp(secret_key):
 
     return response
 app = Flask(__name__)
-
-@app.route('/')
-def home():
-   return render_template('index.html')
-
 if __name__ == '__main__':
     app.run()
